cbb5844a6a3f54c3eed9d1dc74002f48ae28051b
otiaguh <otiagosilva.profissional@gmail.com>
2022-10-01 16:57:52 +0100

    Version Update (1.1)

--- libs/server/server.lua
+++ libs/server/server.lua
@@ -11,18 +11,27 @@ function()
             return print('Tabela vazia.')
         else
             id_discord = string.format("%.f", result.discord_id)
-        
+            id = tonumber(result.id)
+
             account = getAccount(id_discord) or false
             if (account == false) then
                 if (#getAccountsBySerial(serial) > 1) then
                     return 
                 end
                 account = addAccount(id_discord, 'C5B34D56EC038AC2B92DB556E293748BFECDAEF90414EFE08523D7D78F4F4F72')
-                abc:logIn(conta, 'C5B34D56EC038AC2B92DB556E293748BFECDAEF90414EFE08523D7D78F4F4F72')
+                abc:logIn(account, 'C5B34D56EC038AC2B92DB556E293748BFECDAEF90414EFE08523D7D78F4F4F72')
             else
                 account = getAccount(id_discord)
-                abc:logIn(conta, 'C5B34D56EC038AC2B92DB556E293748BFECDAEF90414EFE08523D7D78F4F4F72') 
+                abc:logIn(account, 'C5B34D56EC038AC2B92DB556E293748BFECDAEF90414EFE08523D7D78F4F4F72') 
             end
+
+            query('SELECT approved FROM whitelists WHERE author_id = ?', {result.id}, 
+            function(err, result2, fields)
+                if next(result2) == 0 or result2.approved == 0 then
+                    outputChatBox('vc nao tem whitelist') -- temporary message
+                end
+            end
+            )   
         end
     end
     )
