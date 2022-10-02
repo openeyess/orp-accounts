e4a8cc58b8fa1641831223cd2811ad848266bee6
otiaguh <otiagosilva.profissional@gmail.com>
2022-10-02 21:50:00 +0100

    Login Update

--- libs/server/server.lua
+++ libs/server/server.lua
@@ -2,7 +2,7 @@ require('mysql')
 
 addEventHandler('onPlayerJoin', root,
 function()
-    serial = source:getSerial()
+    serial = getPlayerSerial(source)
     abc = source
 
     query('SELECT * FROM accounts WHERE serial = "?"', {serial}, 
@@ -10,30 +10,31 @@ function()
         if next(result) == 0 then
             return print('Tabela vazia.')
         else
-            id_discord = string.format("%.f", result.discord_id)
             id = tonumber(result.id)
-
-            account = getAccount(id_discord) or false
+            email = tostring(result.email)
+            
+            account = getAccount(email) or false
             if (account == false) then
                 if (#getAccountsBySerial(serial) > 1) then
-                    return 
+                    return
                 end
-                account = addAccount(id_discord, 'C5B34D56EC038AC2B92DB556E293748BFECDAEF90414EFE08523D7D78F4F4F72')
-                abc:logIn(account, 'C5B34D56EC038AC2B92DB556E293748BFECDAEF90414EFE08523D7D78F4F4F72')
+                account = addAccount(email, 'B13A73ED5EDC2913C7B40FCFF2C129FF32261E3B1D2EE69562D74B6B2E8F72F4')
+                logIn(abc, account, 'B13A73ED5EDC2913C7B40FCFF2C129FF32261E3B1D2EE69562D74B6B2E8F72F4')
                 setElementData(abc, 'player_id', id)
             else
-                account = getAccount(id_discord)
-                abc:logIn(account, 'C5B34D56EC038AC2B92DB556E293748BFECDAEF90414EFE08523D7D78F4F4F72')
+                account = getAccount(email)
+                logIn(abc, account, 'B13A73ED5EDC2913C7B40FCFF2C129FF32261E3B1D2EE69562D74B6B2E8F72F4')
                 setElementData(abc, 'player_id', id)
             end
 
-            query('SELECT approved FROM whitelists WHERE author_id = ?', {result.id}, 
+            query('SELECT approved FROM whitelists WHERE author_id = ?', {id},
             function(err, result2, fields)
                 if next(result2) == 0 or result2.approved == 0 then
-                    outputChatBox('vc nao tem whitelist') -- temporary message
+                    print('user sem wl')
+                    -- substituir quando design estiver feito
                 end
             end
-            )   
+            )
         end
     end
     )
